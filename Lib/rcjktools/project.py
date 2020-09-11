b07a1282d8060bc6d75d376b53b0d9e370fe2362
justvanrossum <justvanrossum@gmail.com>
2020-09-11 09:50:00 +0200

    create initial package and previewer script

---
+++ Lib/rcjktools/project.py
@@ -0,0 +1,328 @@
+import logging
+import math
+import re
+from typing import NamedTuple
+import operator
+import pathlib
+
+from fontTools.misc.transform import Transform
+from fontTools.pens.recordingPen import RecordingPointPen
+from fontTools.pens.pointPen import SegmentToPointPen, PointToSegmentPen
+from fontTools.ufoLib.glifLib import readGlyphFromString
+from fontTools.ufoLib.filenames import userNameToFileName
+
+
+class _MathMixin:
+
+    def __add__(self, other):
+        return self._doBinaryOperator(other, operator.add)
+
+    def __sub__(self, other):
+        return self._doBinaryOperator(other, operator.sub)
+
+    def __mul__(self, scalar):
+        return self._doUnaryOperator(scalar, operator.mul)
+
+    def __rmul__(self, scalar):
+        return self._doUnaryOperator(scalar, operator.mul)
+
+
+class MathDict(dict, _MathMixin):
+
+    def _doUnaryOperator(self, scalar, op):
+        result = MathDict()
+        for k, v in self.items():
+            if isinstance(v, (int, float)):
+                result[k] = op(v, scalar)
+            else:
+                result[k] = v
+        return result
+
+    def _doBinaryOperator(self, other, op):
+        # any missing keys will be taken from the other dict
+        self_other = dict(other)
+        self_other.update(self)
+        other_self = dict(self)
+        other_self.update(other)
+        result = MathDict()
+        for k, v1 in self_other.items():
+            v2 = other_self[k]
+            if isinstance(v1, (int, float)):
+                result[k] = op(v1, v2)
+            else:
+                assert v1 == v2, "incompatible dicts"
+                result[k] = v1
+        return result
+
+
+class MathOutline(RecordingPointPen, _MathMixin):
+
+    def isEmpty(self):
+        return not self.value
+
+    def drawPoints(self, pen):
+        self.replay(pen)
+
+    def draw(self, pen):
+        self.drawPoints(PointToSegmentPen(pen))
+
+    def transform(self, t):
+        if not hasattr(t, "transformPoint"):
+            t = Transform(*t)
+        return self.applyUnaryFunc(t.transformPoint)
+
+    def applyUnaryFunc(self, func):
+        result = MathOutline()
+        for m, args, kwargs in self.value:
+            if m == "addPoint":
+                pt, seg, smooth, name = args
+                result.addPoint(func(pt), seg, smooth, name, **kwargs)
+            elif m == "beginPath":
+                result.beginPath()
+            elif m == "endPath":
+                result.endPath()
+            else:
+                assert False, f"unsupported method: {m}"
+        return result
+
+    def _doUnaryOperator(self, scalar, op):
+        def func(pt):
+            x, y = pt
+            return op(x, scalar), op(y, scalar)
+        return self.applyUnaryFunc(func)
+
+    def _doBinaryOperator(self, other, op):
+        result = MathOutline()
+        assert len(self.value) == len(other.value), "incompatible outline"
+        for (m1, args1, kwargs1), (m2, args2, kwargs2) in zip(self.value, other.value):
+            assert m1 == m2, "incompatible outline"
+            if m1 == "addPoint":
+                (x1, y1), seg1, smooth1, name1 = args1
+                (x2, y2), seg2, smooth2, name2 = args2
+                assert seg1 == seg2, "incompatible outline"
+                pt = op(x1, x2), op(y1, y2)
+                result.addPoint(pt, seg1, smooth1, name1, **kwargs1)
+            elif m1 == "beginPath":
+                result.beginPath()
+            elif m1 == "endPath":
+                result.endPath()
+            else:
+                assert False, f"unsupported method: {m}"
+        return result
+
+
+class Glyph:
+
+    def __init__(self):
+        self.outline = MathOutline()
+
+    def getPointPen(self):
+        return self.outline
+
+    def getPen(self):
+        return SegmentToPointPen(self.outline)
+
+    def drawPoints(self, pen):
+        self.outline.drawPoints(pen)
+
+    def draw(self, pen):
+        self.outline.draw(pen)
+
+
+def parseGlyph(p):
+    with open(p) as f:
+        data = f.read()
+    g = Glyph()
+    readGlyphFromString(data, g, g.getPointPen())
+    return g
+
+
+_glyphNamePat = re.compile(rb"<glyph\s+name\s*=\s*\"([^\"]+)\"")
+_unicodePat = re.compile(rb"<unicode\s+hex\s*=\s*\"([^\"]+)\"")
+
+
+class GlyphSet:
+
+    def __init__(self, path):
+        self._path = path
+        self._glyphs = {}
+        self._layers = {}
+
+    def getGlyphNamesAndUnicodes(self):
+        glyphNames = {}
+        for path in self._path.glob("*.glif"):
+            with open(path, "rb") as f:
+                data = f.read(1024)  # assuming all unicodes are in the first 1024 bytes of the file
+            m = _glyphNamePat.search(data)
+            if m is None:
+                raise ValueError(f"invalid .glif file, glyph name not found ({path})")
+            glyphName = m.group(1).decode("utf-8")
+            refFileName = userNameToFileName(glyphName, suffix=".glif")
+            if refFileName != path.name:
+                logging.warning(f"actual file name does not match predicted file name: {refFileName} {path.name} {glyphName}")
+            unicodes = [int(u, 16) for u in _unicodePat.findall(data)]
+            glyphNames[glyphName] = unicodes
+        return glyphNames
+
+    def getGlyph(self, glyphName):
+        glyph = self._glyphs.get(glyphName)
+        if glyph is None:
+            fileName = userNameToFileName(glyphName, suffix=".glif")
+            glyph = parseGlyph(self._path / fileName)
+            self._glyphs[glyphName] = glyph
+        return glyph
+
+    def getLayer(self, layerName):
+        layer = self._layers.get(layerName)
+        if layer is None:
+            layer = GlyphSet(self._path / layerName)
+            self._layers[layerName] = layer
+        return layer
+
+
+class ComponentInfo(NamedTuple):
+
+    name: str
+    coord: dict
+    transform: dict
+
+    def __add__(self, other):
+        return ComponentInfo(self.name, self.coord + other.coord, self.transform + other.transform)
+
+    def __sub__(self, other):
+        return ComponentInfo(self.name, self.coord - other.coord, self.transform - other.transform)
+
+    def __mul__(self, scalar):
+        return ComponentInfo(self.name, self.coord * scalar, self.transform * scalar)
+
+    def __rmul__(self, scalar):
+        return self.__mul__(scalar)
+
+
+def makeTransform(x, y, rotation, scalex, scaley, rcenterx, rcentery):
+    rotation = math.radians(rotation)
+    rcenterx *= scalex
+    rcentery *= scaley
+    t = Transform()
+    t = t.translate(x + rcenterx, y + rcentery)
+    t = t.rotate(rotation)
+    t = t.translate(-rcenterx, -rcentery)
+    t = t.scale(scalex, scaley)
+    return t
+
+
+def _interpolateComponents(glyph, location, varKey):
+    # neutral
+    neutralComponents = []
+    for dc in glyph.lib["robocjk.deepComponents"]:
+        neutralComponents.append(_unpackDeepComponent(dc))
+
+    # variations
+    components = list(neutralComponents)
+    axes = []
+    for axisName, variations in glyph.lib[varKey].items():
+        axes.append((axisName, variations["layerName"], variations["minValue"], variations["maxValue"]))
+        scalar = location.get(axisName, 0)
+        if not scalar:
+            continue
+        varComponents = []
+        for dc in variations["content"]["deepComponents"]:
+            varComponents.append(_unpackDeepComponent(dc))
+        assert len(varComponents) == len(components)
+        for i in range(len(components)):
+            deltaComponent = varComponents[i] - neutralComponents[i]
+            components[i] += scalar * deltaComponent
+    return components, axes
+
+
+def _unpackDeepComponent(dc):
+    name = dc.get("name")
+    coord = dc["coord"]
+    transform = {k: v for k, v in dc.items() if k not in {"coord", "name"}}
+    return ComponentInfo(name, MathDict(coord), MathDict(transform))
+
+
+def _interpolateOutline(glyph, axes, location, glyphSet):
+    glyphName = glyph.name
+    outline = neutralOutline = glyph.outline
+    for axisName, layerName, minValue, maxValue in axes:
+        scalar = location.get(axisName, 0)
+        if not scalar:
+            continue
+        layer = glyphSet.getLayer(layerName)
+        layerGlyph = layer.getGlyph(glyphName)
+        deltaOutline = layerGlyph.outline - neutralOutline
+        outline += scalar * deltaOutline
+    return outline
+
+
+class RoboCJKProject:
+
+    def __init__(self, path):
+        self._path = pathlib.Path(path)
+        self.characterGlyphGlyphSet = GlyphSet(self._path / "characterGlyph")
+        self.deepComponentGlyphSet = GlyphSet(self._path / "deepComponent")
+        self.atomicElementGlyphSet = GlyphSet(self._path / "atomicElement")
+
+    def getGlyphNamesAndUnicodes(self):
+        return self.characterGlyphGlyphSet.getGlyphNamesAndUnicodes()
+
+    def drawCharacterGlyph(self, glyphName, location):
+        glyph = self.characterGlyphGlyphSet.getGlyph(glyphName)
+        components, axes = _interpolateComponents(glyph, location, "robocjk.fontVariationGlyphs")
+        deepItems = []
+        for component in components:
+            deepItem = self.drawDeepComponent(component.name, component.coord, makeTransform(**component.transform))
+            deepItems.append((component.name, deepItem))
+        if glyph.outline.isEmpty():
+            outline = None
+        else:
+            outline = _interpolateOutline(glyph, axes, location, self.characterGlyphGlyphSet)
+        return outline, deepItems
+
+    def drawDeepComponent(self, glyphName, location, transform):
+        glyph = self.deepComponentGlyphSet.getGlyph(glyphName)
+        components, axes = _interpolateComponents(glyph, location, "robocjk.glyphVariationGlyphs")
+        atomicOutlines = []
+        for component in components:
+            t = transform.transform(makeTransform(**component.transform))
+            atomicOutline = self.drawAtomicElement(component.name, component.coord, t)
+            atomicOutlines.append((component.name, atomicOutline))
+        return atomicOutlines
+
+    def drawAtomicElement(self, glyphName, location, transform):
+        glyph = self.atomicElementGlyphSet.getGlyph(glyphName)
+        axes = [(axisName, variations["layerName"], variations["minValue"], variations["maxValue"])
+                for axisName, variations in glyph.lib["robocjk.glyphVariationGlyphs"].items()]
+        outline = _interpolateOutline(glyph, axes, location, self.atomicElementGlyphSet)
+        return outline.transform(transform)
+    
+
+if __name__ == "__main__":
+    # DrawBot test snippet
+    def drawOutline(outline):
+        bez = BezierPath()
+        outline.drawPoints(bez)
+        drawPath(bez)
+
+    project = RoboCJKProject("/Users/just/code/git/BlackFoundry/gs-cjk-rcjk/Hanzi.rcjk/")
+
+    glyphName = "uni3A00"
+    # glyphName = "uni2EBB"
+    # glyphName = "uni3A10"
+    # glyphName = "uni3A69"  # outlines
+    glyphName = "uni540E"  # mix outlines / components
+
+    translate(100, 200)
+    scale(0.8)
+    fill(0, 0.02)
+    stroke(0)
+    steps = 3
+    for i in range(steps):
+        f = i / (steps - 1)
+        outline, deepItems = project.drawCharacterGlyph(glyphName, {"wght": f})
+        if outline is not None:
+            drawOutline(outline)
+        for dcName, atomicOutlines in deepItems:
+            for atomicName, atomicOutline in atomicOutlines:
+                drawOutline(atomicOutline)
---
+++ RoboCJKPreviewer.py
@@ -0,0 +1,165 @@
+import sys
+import os
+import objc
+from AppKit import NSFormatter
+from vanilla import *
+
+import drawBot as db
+from drawBot.ui.drawView import DrawView
+from drawBot.drawBotDrawingTools import _drawBotDrawingTool
+from drawBot.context.drawBotContext import DrawBotContext
+
+try:
+    from rcjktools.project import RoboCJKProject
+except ImportError:
+    libPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), "Lib")
+    assert libPath not in sys.path
+    sys.path.append(libPath)
+    from rcjktools.project import RoboCJKProject
+
+
+def ClassNameIncrementer(clsName, bases, dct):
+    orgName = clsName
+    counter = 0
+    while True:
+        try:
+            objc.lookUpClass(clsName)
+        except objc.nosuchclass_error:
+            break
+        counter += 1
+        clsName = orgName + str(counter)
+    return type(clsName, bases, dct)
+
+
+class UnicodesFormatter(NSFormatter, metaclass=ClassNameIncrementer):
+
+    def stringForObjectValue_(self, unicodes):
+        return " ".join(f"U+{u:04X}" for u in unicodes)
+
+    # def getObjectValue_forString_errorDescription_(self, value, string, error):
+    #     XXX
+
+
+
+class RoboCJKPreview:
+
+    def __init__(self, rcjkProjectPath):
+        self.project = RoboCJKProject(rcjkProjectPath)
+        glyphList = [dict(glyphName=glyphName, unicode=unicodes)
+            for glyphName, unicodes in self.project.getGlyphNamesAndUnicodes().items()]
+        glyphList.sort(key=lambda item: (item["unicode"], item["glyphName"]))
+
+        self.w = Window((1000, 400), "RoboCJKPreview", minSize=(1000, 400), autosaveName="RoboCJKPreview")
+        self.w.axisSlider = Slider((10, 8, 180, 20), value=0, minValue=0, maxValue=1,
+            callback=self.axisSliderCallback)
+
+        top = 40
+        columnDescriptions = [
+            dict(title="glyph name", key="glyphName"),
+            dict(title="unicode", key="unicode", formatter=UnicodesFormatter.alloc().init()),
+        ]
+        self.w.characterGlyphList = List((0, top, 200, 0), glyphList,
+            columnDescriptions=columnDescriptions,
+            allowsMultipleSelection=False,
+            drawFocusRing=False,
+            selectionCallback=self.characterGlyphListSelectionChangedCallback)
+
+        self.w.deepComponentList = List((200, top, 200, 0), [],
+            allowsMultipleSelection=False,
+            drawFocusRing=False,
+            selectionCallback=self.deepComponentListSelectionChangedCallback)
+
+        self.w.atomicElementList = List((400, top, 200, 0), [],
+            allowsMultipleSelection=False,
+            drawFocusRing=False,
+            selectionCallback=self.atomicElementListSelectionChangedCallback)
+
+        self.w.dbView = DrawView((600, 0, 0, 0))  # The DrawBot PDF view
+        self.w.characterGlyphList.setSelection([])
+        self.w.open()
+
+    def characterGlyphListSelectionChangedCallback(self, sender):
+        self.updateCurrentGlyph()
+        deepComponents = [dcName for dcName, atomicElements in self._currentGlyphComponents]
+        self.w.deepComponentList.set(deepComponents)
+        self.w.deepComponentList.setSelection([])
+        self.w.atomicElementList.set([])
+        self.w.atomicElementList.setSelection([])
+
+    def updateCurrentGlyph(self):
+        sel = self.w.characterGlyphList.getSelection()
+        if sel:
+            glyphName = self.w.characterGlyphList[sel[0]]["glyphName"]
+            outline, dcItems = self.project.drawCharacterGlyph(
+                glyphName, location={"wght": self.w.axisSlider.get()})
+        else:
+            outline = None
+            dcItems = []
+        self._currentGlyphOutline = outline
+        self._currentGlyphComponents = dcItems
+
+    def deepComponentListSelectionChangedCallback(self, sender):
+        sel = sender.getSelection()
+        if sel:
+            dcName, atomicElements = self._currentGlyphComponents[sel[0]]
+            aeNames = [aeName for aeName, aeOutline in atomicElements]
+            self.w.atomicElementList.set(aeNames)
+        else:
+            self.w.atomicElementList.set([])
+        self.w.atomicElementList.setSelection([])
+
+    def atomicElementListSelectionChangedCallback(self, sender):
+        self.drawCurrentGlyph()
+        self.displayDrawing()
+
+    def axisSliderCallback(self, sender):
+        self.updateCurrentGlyph()
+        self.drawCurrentGlyph()
+        self.displayDrawing()
+
+    def drawCurrentGlyph(self):
+        db.newDrawing()
+        db.translate(100, 100)
+        db.scale(0.8)
+        db.fill(None)
+        db.stroke(0.2, 0.3, 1)
+        db.rect(0, 0, 1000, 1000)
+        db.stroke(None)
+        db.translate(0, 150)  # ???
+        db.fill(0, 1, 0, 0.3)
+        db.stroke(0)
+        dcSelection = set(self.w.deepComponentList.getSelection())
+        aeSelection = set(self.w.atomicElementList.getSelection())
+        if self._currentGlyphOutline is not None:
+            drawOutline(self._currentGlyphOutline)
+        if self._currentGlyphComponents:
+            for dcIndex, (dcName, atomicElements) in enumerate(self._currentGlyphComponents):
+                for aeIndex, (aeName, atomicOutline) in enumerate(atomicElements):
+                    if dcIndex in dcSelection:
+                        if aeIndex in aeSelection:
+                            db.fill(1, 0, 0, 0.3)
+                        else:
+                            db.fill(0, 0, 1, 0.3)
+                    else:
+                        db.fill(0, 0.3)
+                    drawOutline(atomicOutline)
+        db.endDrawing()
+
+    def displayDrawing(self):
+        context = DrawBotContext()
+        _drawBotDrawingTool._drawInContext(context)
+        self.w.dbView.setPDFDocument(context.getNSPDFDocument())
+
+
+def drawOutline(outline):
+    bez = db.BezierPath()
+    outline.drawPoints(bez)
+    db.drawPath(bez)
+
+
+if __name__ == "__main__":
+    from vanilla.dialogs import getFolder
+    result = getFolder("Please select a .rcjk project folder")
+    if result:
+        RoboCJKPreview(result[0])
+    # uni348A interpol bug
