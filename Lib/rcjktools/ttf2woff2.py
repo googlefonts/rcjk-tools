4ea735feaf5d3f5010ee1814d34736713b792303
justvanrossum <justvanrossum@gmail.com>
2021-08-30 12:06:07 +0200

    add support for multiple input arguments, and an --output-dir option

--- Lib/rcjktools/ttf2woff2.py
+++ Lib/rcjktools/ttf2woff2.py
@@ -1,13 +1,32 @@
+import argparse
 import pathlib
 import sys
 from fontTools.ttLib import TTFont
 
 
 def main():
-    p = pathlib.Path(sys.argv[1])
-    f = TTFont(p)
-    f.flavor = "woff2"
-    f.save(p.parent / (p.stem + ".woff2"))
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fonts", nargs="+", help="One or more TTF or OTF fonts")
+    parser.add_argument("--output-dir", help="The output dir for the compressed fonts")
+
+    args = parser.parse_args()
+
+    if args.output_dir:
+        outputDir = pathlib.Path(args.output_dir).resolve()
+        outputDir.mkdir(parents=True, exist_ok=True)
+    else:
+        outputDir = None
+
+    for p in args.fonts:
+        p = pathlib.Path(p).resolve()
+        parentFolder = outputDir if outputDir is not None else p.parent
+        fileName = p.stem + ".woff2"
+        outputPath = parentFolder / fileName
+        print("source:", p)
+        print("destination:", outputPath)
+        f = TTFont(p)
+        f.flavor = "woff2"
+        f.save(outputPath)
 
 
 if __name__ == "__main__":
